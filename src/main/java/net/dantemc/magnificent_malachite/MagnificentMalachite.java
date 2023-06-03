aa9a12742edd48f42725717b25e5643fca8aa9ec
DanteMinecraft <danteminecraft2018@gmail.com>
2023-06-03 21:38:16 +0200

    Malachite has been added

--- src/main/java/net/dantemc/magnificent_malachite/MagnificentMalachite.java
+++ src/main/java/net/dantemc/magnificent_malachite/MagnificentMalachite.java
@@ -1,9 +1,21 @@
 package net.dantemc.magnificent_malachite;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class MagnificentMalachite implements ModInitializer {
-	public static final String MOD_ID = "magnificent_malachite";
+
+	//ITEMS
+	public static final Item MALACHITE =
+			Registry.register(Registries.ITEM, new Identifier("magnificent_malachite", "malachite"),
+					new Item(new FabricItemSettings()));
+
+	//BLOCKS
+
 	@Override
 	public void onInitialize() {
 	}
