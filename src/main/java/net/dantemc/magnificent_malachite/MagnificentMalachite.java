ba09de59f6f6a1dfbf46ee3166d88283b286c736
DanteMinecraft <danteminecraft2018@gmail.com>
2023-06-03 23:09:24 +0200

    Malachite has been added

--- src/main/java/net/dantemc/magnificent_malachite/MagnificentMalachite.java
+++ src/main/java/net/dantemc/magnificent_malachite/MagnificentMalachite.java
@@ -2,21 +2,39 @@ package net.dantemc.magnificent_malachite;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class MagnificentMalachite implements ModInitializer {
 
-	//ITEMS
-	public static final Item MALACHITE =
-			Registry.register(Registries.ITEM, new Identifier("magnificent_malachite", "malachite"),
+	//creative tab
+	private static final ItemGroup MAGNIFICENT_MALACHITE = FabricItemGroup.builder(
+			new Identifier("magnificent_malachite", "magnificent_malachite"))
+			.icon(() -> new ItemStack(MagnificentMalachite.RAW_MALACHITE)).build();
+
+	//items
+	public static final Item RAW_MALACHITE =
+			Registry.register(Registries.ITEM, new Identifier("magnificent_malachite", "raw_malachite"),
 					new Item(new FabricItemSettings()));
 
-	//BLOCKS
+
+	//blocks
 
 	@Override
 	public void onInitialize() {
+
+		//item to item group
+		ItemGroupEvents.modifyEntriesEvent(MAGNIFICENT_MALACHITE).register(content -> {
+			content.add(RAW_MALACHITE);
+		});
+
+		//continue
 	}
 }
